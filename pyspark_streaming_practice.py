7230bd241cb368c87331c380015881081b105c22
farsim <farsimhossain@gmail.com>
2023-12-26 10:20:10 -0500

    reading from kafka added

--- pyspark_streaming_practice.py
+++ pyspark_streaming_practice.py
@@ -13,3 +13,13 @@ df = spark.readStream \
     .option("port", "9090") \
     .load()
 
+# reading from Kafka example 
+# in the options, we have to give the hostname and other things like since what point it will read
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "192.168.1.100:9092") \
+    .option("subscribe", "json_topic") \
+    .option("startingOffset", "earliest") \
+    .load()
+
+
